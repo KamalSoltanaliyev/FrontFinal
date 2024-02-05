80e0bfffa47badb7b7f6fdfe0847d8f7b2426733
KamalSoltanaliyev <kamalas@code.edu.az>
2024-02-05 18:55:29 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('menu-icon').addEventListener('click', function() {
+//   var menuContent = document.getElementById('menu-content');
+//   var overlay = document.getElementById('overlay');
+
+//   menuContent.classList.toggle('show-menu');
+//   overlay.classList.toggle('show-overlay');
+// });
+
+// document.getElementById('overlay').addEventListener('click', function() {
+//   var menuContent = document.getElementById('menu-content');
+//   var overlay = document.getElementById('overlay');
+
+//   menuContent.classList.remove('show-menu');
+//   overlay.classList.remove('show-overlay');
+// });
+
+
+
+
+
+let countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 14);
+
+let x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("Countdown").innerHTML =
+    "<div class='time'><h6>" +
+    days +
+    "</h6><span>days</span></div>" +
+    "<div class='time'><h6>" +
+    hours +
+    "</h6><span>hours</span></div>" +
+    "<div class='time'><h6>" +
+    minutes +
+    "</h6><span>minutes</span></div>" +
+    "<div class='time'><h6>" +
+    seconds +
+    "</h6><span>seconds</span></div>";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
